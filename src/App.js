11ffd797b3bc35961feb3ec5bdcef3919d022bc9
Nipun Kohli <nipunk75@gmail.com>
2020-01-01 14:38:42 +0530

    upload now

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends Component {
         fav: "",
         favalbum: "",
         tempo: "",
-        albums: []
+        MyName: "",
+        MyTop: [],
+        Recently: ""
       }
     };
   }
@@ -39,27 +41,44 @@ class App extends Component {
     return hashParams;
   }
 
+  addToFEL() {
+    spotifyApi.getMyCurrentPlaybackState().then(data => {
+      spotifyApi
+        .addTracksToPlaylist("6qv5jMWDQLAOzduW1ZH4cr", [data.item.uri])
+        .then(add => {
+          console.log(add.snapshot_id);
+        });
+    });
+  }
+
   getNowPlaying() {
     spotifyApi.getMyCurrentPlaybackState().then(data => {
       spotifyApi.getTrack("31VOknKjFrEX47bZXzqcoF").then(dat => {
-        spotifyApi
-          .getAudioAnalysisForTrack("31VOknKjFrEX47bZXzqcoF")
-          .then(d => {
-            spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE").then(alb => {
-              console.log(alb.items[0].name);
-              this.setState({
-                nowPlaying: {
-                  name: data.item.name,
-                  albumArt: data.item.album.images[0].url,
-                  album: data.item.album.name,
-                  fav: dat.name,
-                  favalbum: dat.album.images[0].url,
-                  tempo: d.track.tempo,
-                  albums: alb.body
-                }
+        spotifyApi.getAudioAnalysisForTrack(data.item.id).then(d => {
+          spotifyApi
+            .getMyTopTracks({ limit: 3, time_range: "short_term" })
+            .then(top => {
+              spotifyApi.getMyRecentlyPlayedTracks({ after: 50 }).then(rec => {
+                let temptop = [];
+                top.items.map(track => {
+                  temptop.push(track.name);
+                });
+                this.setState({
+                  nowPlaying: {
+                    name: data.item.name,
+                    albumArt: data.item.album.images[0].url,
+                    album: data.item.album.name,
+                    fav: dat.name,
+                    favalbum: dat.album.images[0].url,
+                    tempo: d.track.tempo,
+                    MyTop: temptop,
+                    Recently: rec.items[0].track.name
+                  }
+                });
+                console.log(this.state.Recently);
               });
             });
-          });
+        });
       });
     });
   }
@@ -75,15 +94,26 @@ class App extends Component {
         <section id="main2">
           <h1>Now Playing: {this.state.nowPlaying.name}</h1>
           <h1>Album : {this.state.nowPlaying.album}</h1>
+          <h1>Previous Song : {this.state.nowPlaying.Recently}</h1>
+
           <img
             id="im"
             src={this.state.nowPlaying.albumArt}
             style={{ height: 150 }}
           />
           <div id="temp">
+            <h1>
+              My Top Tracks:{" "}
+              {this.state.nowPlaying.MyTop.map(pop => (
+                <ul>
+                  <li>{pop}</li>
+                </ul>
+              ))}
+              <br></br>
+            </h1>
+
             <h1>Tempo :{this.state.nowPlaying.tempo}</h1>
           </div>
-          {/* {this.getFav()}; */}
           <div className="fav">
             <h1>Favorite track: {this.state.nowPlaying.fav}</h1>
             <img src={this.state.nowPlaying.favalbum} style={{ height: 150 }} />
@@ -94,6 +124,16 @@ class App extends Component {
             {this.state.loggedIn && (
               <button onClick={() => this.getNowPlaying()}>
                 <h1>Check Now Playing</h1>
+              </button>
+            )}
+            <hr></hr>
+          </div>
+        </div>
+        <div className="container1">
+          <div id="main-header3">
+            {this.state.loggedIn && (
+              <button onClick={() => this.addToFEL()}>
+                <h1>Add to playlist</h1>
               </button>
             )}
             <hr></hr>
